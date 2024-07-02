8d3d5ed9fe5a56a9e1a3a462a83e3810c8255bea
qiao1025566574 <18811076159@163.com>
2024-07-02 10:08:28 +0800

    20240702更新

--- qiao.sh
+++ qiao.sh
@@ -1,8 +1,11 @@
 # day
-cp ../notion/time/png/new_20240630*.png ./png/.
+day_num=20240701
+
+cp ../notion/time/png/new_${day_num}*.png ./png/.
+ls -lh ./png/new_${day_num}*.png
+
 # week
 #cp ../notion/time/png/202406week05_*show.png ./png/.
 # month
-cp ../notion/time/png/202406_pie.png ./png/.
-cp ../notion/time/png/202406_work_time.png ./png/.
-ls -lh ./png/*.png
+#cp ../notion/time/png/202406_pie.png ./png/.
+#cp ../notion/time/png/202406_work_time.png ./png/.
