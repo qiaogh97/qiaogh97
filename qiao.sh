d7c2c20e140c1464b42ac967ac055bfaa8ac8728
qiao1025566574 <18811076159@163.com>
2024-07-08 06:01:29 +0800

    20240707 update

--- qiao.sh
+++ qiao.sh
@@ -1,11 +1,11 @@
 # day
-day_num=20240706
+day_num=20240707
 
 cp ../notion/time/png/new_${day_num}*.png ./png/.
 ls -lh ./png/new_${day_num}*.png
 
 # week
-cp ../notion/time/png/202407week01_*show.png ./png/.
+#cp ../notion/time/png/202407week01_*show.png ./png/.
 # month
 #cp ../notion/time/png/202406_pie.png ./png/.
 #cp ../notion/time/png/202406_work_time.png ./png/.
