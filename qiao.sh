bafcf8d66256f9a0d6aa7cb190975d0da63a2b33
qiao1025566574 <18811076159@163.com>
2024-08-27 10:01:57 +0800

    20240827 update

--- qiao.sh
+++ qiao.sh
@@ -1,14 +1,14 @@
 # day
-day_num=20240825
+day_num=20240826
 
-#cp ../notion/time/png/new_${day_num}*.png ./png/.
-#ls -lh ./png/new_${day_num}*.png
+cp ../notion/time/png/new_${day_num}*.png ./png/.
+ls -lh ./png/new_${day_num}*.png
 
 # week
-week_num=04
-cp ../notion/time/png/202408week${week_num}_*show.png ./png/.
-ls -lh ./png/202408week${week_num}_*show.png
+week_num=05
+#cp ../notion/time/png/202408week${week_num}_*show.png ./png/.
+#ls -lh ./png/202408week${week_num}_*show.png
 # month
-month_num=07
+month_num=08
 #cp ../notion/time/png/2024${month_num}_pie.png ./png/.
 #cp ../notion/time/png/2024${month_num}_work_time.png ./png/.
