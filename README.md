8ecb02d3823ce99ee0b690092930be43a5866f2a
qiao1025566574 <18811076159@163.com>
2024-10-22 10:05:26 +0800

    20241022 update

--- README.md
+++ README.md
@@ -15,8 +15,8 @@
 - 🌍热爱阳明心学, 一生所求，此心光明
 
 ## 📈 数据是不会说谎的
-### 日时间统计(2024.10.20)
-<img src='./png/new_20241020_pie.png' width="50%"><img src='./png/new_20241020_plot.png' width="50%">
+### 日时间统计(2024.10.21)
+<img src='./png/new_20241021_pie.png' width="50%"><img src='./png/new_20241021_plot.png' width="50%">
 
 ### 周时间统计(2024.10.13-2024.10.20)
 <img src='./png/202410week03_table_show.png' width="50%"><img src='./png/202410week03_emotion_show.png' width="50%">
--- qiao.sh
+++ qiao.sh
@@ -1,4 +1,4 @@
-day=20
+day=21
 
 year=2024
 month=10
@@ -7,12 +7,12 @@ week=03
 day_num=${year}${month}${day}
 
 # day
-#cp ../notion/time/png/new_${day_num}*.png ./png/.
-#ls -lh ./png/new_${day_num}*.png
+cp ../notion/time/png/new_${day_num}*.png ./png/.
+ls -lh ./png/new_${day_num}*.png
 
 # week
-cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
-ls -lh ./png/${year}${month}week${week}_*show.png
+#cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
+#ls -lh ./png/${year}${month}week${week}_*show.png
 
 # month
 #cp ../notion/time/png/${year}${month}_pie.png ./png/.
