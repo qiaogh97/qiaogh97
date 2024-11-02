abc78d18d1a2366a56ca945c1498e668ee0a9fd1
qiao1025566574 <18811076159@163.com>
2024-11-02 09:22:00 +0800

    20241102 update

--- qiao.sh
+++ qiao.sh
@@ -1,20 +1,20 @@
-day=31
+day=01
 
 year=2024
-month=10
-week=04
+month=11
+week=01
 
 day_num=${year}${month}${day}
 
 # day
-#cp ../notion/time/png/new_${day_num}*.png ./png/.
-#ls -lh ./png/new_${day_num}*.png
+cp ../notion/time/png/new_${day_num}*.png ./png/.
+ls -lh ./png/new_${day_num}*.png
 
 # week
 #cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
 #ls -lh ./png/${year}${month}week${week}_*show.png
 
 # month
-cp ../notion/time/png/${year}${month}_pie.png ./png/.
-cp ../notion/time/png/${year}${month}_work_time.png ./png/.
-ls -lh ./png/${year}${month}*.png
+#cp ../notion/time/png/${year}${month}_pie.png ./png/.
+#cp ../notion/time/png/${year}${month}_work_time.png ./png/.
+#ls -lh ./png/${year}${month}*.png
