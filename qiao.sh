d6c2bea5fc83d99f4cf2a85620e0f201c59c9a3d
qiaogh <18811076159@163.com>
2025-04-24 10:39:07 +0800

    20250424

--- qiao.sh
+++ qiao.sh
@@ -1,4 +1,4 @@
-day=22
+day=23
 
 year=2025
 month=04
@@ -7,12 +7,12 @@ week=03
 day_num=${year}${month}${day}
 
 # day
-#cp ../notion/time/png/new_${day_num}*.png ./png/.
-#ls -lh ./png/new_${day_num}*.png
+cp ../notion/time/png/new_${day_num}*.png ./png/.
+ls -lh ./png/new_${day_num}*.png
 
 # week
-cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
-ls -lh ./png/${year}${month}week${week}_*show.png
+#cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
+#ls -lh ./png/${year}${month}week${week}_*show.png
 
 # month
 #cp ../notion/time/png/${year}${month}_pie.png ./png/.
