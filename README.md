6b02e6b17bf3d94f2a1613e6c54bb4b240fcf604
qiaogh <18811076159@163.com>
2025-09-09 15:44:04 +0800

    daily update

--- README.md
+++ README.md
@@ -15,8 +15,8 @@
 - 🌍心如明镜,物来则照;万物一体,吾性具足;任它千变万化,我心自岿然不动;浩浩乎照见世间万物而不失其本体,荡荡乎如庖丁解牛游刃有余
 
 ## 📈 数据是不会说谎的
-### 日时间统计(2025.09.06)
-<img src='./png/new_20250906_pie.png' width="50%"><img src='./png/new_20250906_plot.png' width="50%">
+### 日时间统计(2025.09.08)
+<img src='./png/new_20250908_pie.png' width="50%"><img src='./png/new_20250908_plot.png' width="50%">
 
 ### 周时间统计(2025.08.31-2025.09.06)
 <img src='./png/202509week01_table_show.png' width="50%"><img src='./png/202509week01_emotion_show.png' width="50%">
--- qiao.sh
+++ qiao.sh
@@ -1,4 +1,4 @@
-day=06
+day=08
 
 year=2025
 month=09
@@ -7,12 +7,12 @@ week=01
 day_num=${year}${month}${day}
 
 # day
-#cp ../notion/time/png/new_${day_num}*.png ./png/.
-#ls -lh ./png/new_${day_num}*.png
+cp ../notion/time/png/new_${day_num}*.png ./png/.
+ls -lh ./png/new_${day_num}*.png
 
 # week
-cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
-ls -lh ./png/${year}${month}week${week}_*show.png
+#cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
+#ls -lh ./png/${year}${month}week${week}_*show.png
 
 # month
 #cp ../notion/time/png/${year}${month}_pie.png ./png/.
