1d4b1846b50276f6b0756152e40b066223edeaf2
qiaogh <18811076159@163.com>
2026-03-02 11:37:13 +0800

    daily update

--- qiao.sh
+++ qiao.sh
@@ -1,20 +1,23 @@
-day=25
+day=01
 
 year=2026
 month=02
-week=03
+week=04
 
 day_num=${year}${month}${day}
 
 # day
-cp ../notion/time/png/new_${day_num}*.png ./png/.
-ls -lh ./png/new_${day_num}*.png
+#cp ../notion/time/png/new_${day_num}*.png ./png/.
+#ls -lh ./png/new_${day_num}*.png
 
 # week
-#cp ../notion/time/png/${year}${month}week${week}_*show.png ./png/.
-#ls -lh ./png/${year}${month}week${week}_*show.png
+cp ../notion/time/png/${year}${month}week${week}_table.png ./png/.
+cp ../notion/time/png/${year}${month}week${week}_emotion.png ./png/.
+cp ../notion/time/png/${year}${month}week${week}_location_pie.png ./png/.
+ls -lh ./png/${year}${month}week${week}_*.png
 
 # month
 #cp ../notion/time/png/${year}${month}_pie.png ./png/.
+#cp ../notion/time/png/${year}${month}_location_pie.png ./png/.
 #cp ../notion/time/png/${year}${month}_work_time.png ./png/.
 #ls -lh ./png/${year}${month}*.png
