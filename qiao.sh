22316c354d4455cd60a37df99990c84ab65a64b6
qiaogh <18811076159@163.com>
2026-04-07 10:54:29 +0800

    daily update

--- qiao.sh
+++ qiao.sh
@@ -1,4 +1,4 @@
-day=02
+day=06
 
 year=2026
 month=04
@@ -11,10 +11,10 @@ cp ../notion/time/png/new_${day_num}*.png ./png/.
 ls -lh ./png/new_${day_num}*.png
 
 # week
-#cp ../notion/time/png/${year}${month}week${week}_table.png ./png/.
-#cp ../notion/time/png/${year}${month}week${week}_emotion.png ./png/.
-#cp ../notion/time/png/${year}${month}week${week}_location_pie.png ./png/.
-#ls -lh ./png/${year}${month}week${week}_*.png
+cp ../notion/time/png/${year}${month}week${week}_table.png ./png/.
+cp ../notion/time/png/${year}${month}week${week}_emotion.png ./png/.
+cp ../notion/time/png/${year}${month}week${week}_location_pie.png ./png/.
+ls -lh ./png/${year}${month}week${week}_*.png
 
 # month
 #cp ../notion/time/png/${year}${month}_pie.png ./png/.
